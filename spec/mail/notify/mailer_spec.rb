22cb87b3170d84de63fd7ccfe62c5d876505ba69
meyric <meyric@dxw.com>
2024-02-22 14:21:42 +0000

    Update the Mailer
    The Mailer is what is used to provide applcations with Notify emails.
    
    This class is used as the parent for new subclasses that provide the
    application specific emails.
    
    Like a module, we have to test this by creating a new TestMailer that
    inherits from our updated class.
    
    The main thing to notice here is that we have to prevent Rails from
    carrying out the default behaviour of looking for views to provide the
    parts of the email (the html and plain text content), we do so by
    calling `mail` with a block that sets the parts to empty object - these
    are then replaced later with the actual content.

--- spec/mail/notify/mailer_spec.rb
+++ spec/mail/notify/mailer_spec.rb
@@ -1,53 +1,219 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require "mailers/test_mailer"
 
 RSpec.describe Mail::Notify::Mailer do
-  let(:mailer) { Mail::Notify::Mailer.new }
+  describe "#view_mail" do
+    it "sets the message template id" do
+      message_params = {template_id: "template-id", to: "test.name@email.co.uk", subject: "Test subject"}
 
-  context "with a view" do
-    it "sends the template" do
-      expect(mailer).to receive(:mail).with({template_id: "foo", bar: "baz"})
-      mailer.view_mail("foo", bar: "baz")
+      message = TestMailer.with(message_params).test_view_mail
+
+      expect(message.template_id).to eql("template-id")
     end
 
-    it "raises an error if the template ID is blank" do
-      expect { mailer.view_mail("", bar: "baz") }.to raise_error(
-        ArgumentError,
-        "You must specify a template ID"
+    it "sets the message subject" do
+      message_params = {template_id: "template-id", to: "test.name@email.co.uk", subject: "Test subject"}
+
+      message = TestMailer.with(message_params).test_view_mail
+
+      expect(message.header[:subject]).to be_a Mail::Field
+      expect(message.header[:subject].value).to eql("Test subject")
+    end
+
+    it "sets the message to address" do
+      message_params = {template_id: "template-id", to: "test.name@email.co.uk", subject: "Test subject"}
+
+      message = TestMailer.with(message_params).test_view_mail
+
+      expect(message.header[:to]).to be_a Mail::Field
+      expect(message.header[:to].value).to eql("test.name@email.co.uk")
+    end
+
+    it "sets the subject on personalisation" do
+      message_params = {template_id: "template-id", to: "test.name@email.co.uk", subject: "Test subject"}
+
+      message = TestMailer.with(message_params).test_view_mail
+
+      expect(message.personalisation[:subject]).to eql("Test subject")
+    end
+
+    it "sets the body on personalisation" do
+      message_params = {template_id: "template-id", to: "test.name@email.co.uk", subject: "Test subject"}
+
+      message = TestMailer.with(message_params).test_view_mail
+
+      expect(message.personalisation[:body]).to eql("This is the body from the mailers view.\r\n")
+    end
+
+    it "drops any further personalisation" do
+      message_params = {
+        template_id: "template-id",
+        to: "test.name@email.co.uk",
+        subject: "Test subject",
+        personalisation: {name: "Test Name"}
+      }
+
+      message = TestMailer.with(message_params).test_view_mail
+
+      expect(message.personalisation.key?(:name)).to be false
+    end
+
+    it "sets the reply_to_id" do
+      message_params = {
+        template_id: "template-id",
+        to: "test.name@email.co.uk",
+        subject: "Test subject",
+        reply_to_id: "test-reply-to-id"
+      }
+
+      message = TestMailer.with(message_params).test_view_mail
+
+      expect(message.reply_to_id).to eql("test-reply-to-id")
+    end
+
+    it "sets the reference" do
+      message_params = {
+        template_id: "template-id",
+        to: "test.name@email.co.uk",
+        subject: "Test subject",
+        reference: "test-reference"
+      }
+
+      message = TestMailer.with(message_params).test_view_mail
+
+      expect(message.reference).to eql("test-reference")
+    end
+
+    it "requires template id to be set" do
+      message_params = {to: "test.name@email.co.uk", subject: "Test subject"}
+
+      message = TestMailer.with(message_params).test_view_mail
+
+      expect { message.template_id }.to raise_error(
+        ArgumentError, "You must specify a Notify template ID"
+      )
+    end
+
+    it "requires to address to be set" do
+      message_params = {template_id: "template-id", subject: "Test subject"}
+
+      message = TestMailer.with(message_params).test_view_mail
+
+      expect { message.header[:to] }.to raise_error(
+        ArgumentError, "You must supply a to address"
+      )
+    end
+
+    it "requires subject to be set" do
+      message_params = {template_id: "template-id", to: "Test subject"}
+
+      message = TestMailer.with(message_params).test_view_mail
+
+      expect { message.header[:subject] }.to raise_error(
+        ArgumentError, "You must specify a subject"
       )
     end
   end
 
-  context "with a template" do
-    it "sends a blank body and subject" do
-      expect(mailer).to receive(:mail).with({
-        template_id: "foo",
-        bar: "baz",
-        body: "",
-        subject: ""
-      })
-      mailer.template_mail("foo", bar: "baz")
+  describe "#template_email" do
+    it "sets the message template id" do
+      message_params = {template_id: "template-id", to: "test.name@email.co.uk"}
+
+      message = TestMailer.with(message_params).test_template_mail
+
+      expect(message.template_id).to eql("template-id")
     end
 
-    it "raises an error if the template ID is blank" do
-      expect { mailer.template_mail("", bar: "baz") }.to raise_error(
-        ArgumentError,
-        "You must specify a template ID"
-      )
+    it "sets the message to address" do
+      message_params = {template_id: "template-id", to: "test.name@email.co.uk"}
+
+      message = TestMailer.with(message_params).test_template_mail
+
+      expect(message.header[:to]).to be_a Mail::Field
+      expect(message.header[:to].value).to eql("test.name@email.co.uk")
+    end
+
+    it "sets the subject as ActionMailer requires one" do
+      message_params = {template_id: "template-id", to: "test.name@email.co.uk"}
+
+      message = TestMailer.with(message_params).test_template_mail
+
+      expect(message.header[:subject]).to be_a Mail::Field
+      expect(message.header[:subject].value).to eql("Subject managed in Notify")
+    end
+
+    it "sets the subject if one is passed, even though it will not be used" do
+      message_params = {template_id: "template-id", to: "test.name@email.co.uk", subject: "My subject"}
+
+      message = TestMailer.with(message_params).test_template_mail
+
+      expect(message.header[:subject]).to be_a Mail::Field
+      expect(message.header[:subject].value).to eql("My subject")
+    end
+
+    context "without personalisation" do
+      it "sets the personalisation of the message to an empty hash" do
+        message_params = {template_id: "template-id", to: "test.name@email.co.uk"}
+
+        message = TestMailer.with(message_params).test_template_mail
+
+        expect(message.personalisation).to eql({})
+      end
+    end
+
+    context "with personalisation" do
+      it "sets the personalisation of the message" do
+        message_params = {
+          template_id: "template-id",
+          to: "test.name@email.co.uk",
+          personalisation: {name: "Name", age: "25"}
+        }
+
+        message = TestMailer.with(message_params).test_template_mail
+
+        expect(message.personalisation).to eql({name: "Name", age: "25"})
+      end
+    end
+
+    it "sets the reply_to_id" do
+      message_params = {
+        template_id: "template-id",
+        to: "test.name@email.co.uk",
+        subject: "Test subject",
+        reply_to_id: "test-reply-to-id"
+      }
+
+      message = TestMailer.with(message_params).test_template_mail
+
+      expect(message.reply_to_id).to eql("test-reply-to-id")
+    end
+
+    it "sets the reference" do
+      message_params = {
+        template_id: "template-id",
+        to: "test.name@email.co.uk",
+        subject: "Test subject",
+        reference: "test-reference"
+      }
+
+      message = TestMailer.with(message_params).test_template_mail
+
+      expect(message.reference).to eql("test-reference")
     end
   end
 
   describe "#blank_allowed" do
     context "when given a non-blank value" do
       it "returns the given value" do
-        expect(mailer.blank_allowed("foo")).to eq "foo"
+        expect(TestMailer.new.blank_allowed("foo")).to eq "foo"
       end
     end
 
     context "when given a blank value" do
       it "returns explicitly blank personalisation" do
-        expect(mailer.blank_allowed("")).to be Mail::Notify::Personalisation::BLANK
+        expect(TestMailer.new.blank_allowed("")).to be Mail::Notify::Personalisation::BLANK
       end
     end
   end
