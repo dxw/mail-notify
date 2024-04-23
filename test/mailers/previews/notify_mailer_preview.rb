f03ae69109136407cc748831d03a508418af4932
meyric <meyric@dxw.com>
2024-04-23 09:07:08 +0100

    Add integration tests for blank personalisation
    We want to make sure that using `blank_allowed` for personalisation
    works end to end, so we add coverage for it in our integration tests.

--- test/mailers/previews/notify_mailer_preview.rb
+++ test/mailers/previews/notify_mailer_preview.rb
@@ -10,4 +10,8 @@ class NotifyMailerPreview < ActionMailer::Preview
   def template_email_with_personalisation
     NotifyMailer.with(to: "template.email@notifications.service.gov.uk", name: "Test Name").template_with_personalisation
   end
+
+  def template_email_with_blank_personalisation
+    NotifyMailer.with(to: "template.email@notifications.service.gov.uk", name: nil).template_with_blank_personalisation
+  end
 end
