c8cbbc5bd4f1dee1bd1c73e56729cda0e27119cc
Phil Lee <asmega@users.noreply.github.com>
2024-12-03 12:23:37 +0000

    support one click unsubscribe

--- lib/mail/notify/message.rb
+++ lib/mail/notify/message.rb
@@ -3,7 +3,7 @@
 module Mail
   module Notify
     module Message
-      attr_accessor :template_id, :personalisation, :reply_to_id, :reference
+      attr_accessor :template_id, :personalisation, :reply_to_id, :reference, :one_click_unsubscribe_url
     end
   end
 end
